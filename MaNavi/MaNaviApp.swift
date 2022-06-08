87e18c194d28c9ad187f636be9b8d10c39ab2146
taka-2120 <yu_tkhs@icloud.com>
2022-06-09 00:29:14 +0900

    Initial Commit

---
+++ MaNavi/MaNaviApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaNaviApp.swift
+//  MaNavi
+//
+//  Created by Yu Takahashi on 2022/06/09.
+//
+
+import SwiftUI
+
+@main
+struct MaNaviApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
